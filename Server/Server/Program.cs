6966f081768d95e2f145b5bc23c4e2ca089d2146
심재천 <shimnaldo98@gmail.com>
2025-05-12 18:59:36 +0900

    feat : Listener

---
+++ Server/Server/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string host = Dns.GetHostName();
+            // 호스트의 이름으로 로컬 IP 
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            // AddressList : 유선/무선, VPN, 가상 네트워크 어댑터가 모두 포함된 배열
+            // AddressFamily
+            //  - InterNetwork : IPv4 
+            //  - InterNetworkV6 : IPv6 
+            IPAddress ipAddr
+                = ipHost.AddressList.First(x => x.AddressFamily == AddressFamily.InterNetwork);
+
+            // IP + 포트, 7777(시스템 예약 포트를 피한 테스트 포트)
+            // 0~1023 : (시스템 예약 포트)
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+        }
+    }
+}
---
+++ Server/ServerCore/Listener.cs
@@ -0,0 +1,55 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace ServerCore
+{
+    public class Listener
+    {
+        private Socket _listenSocket;
+
+        /// <param name="register"> 비동기 요청 개수 </param>
+        /// <param name="backlog"> 버퍼링 최대개수 </param>
+        public void Init(IPEndPoint endPoint, int register = 10, int backlog = 100)
+        {
+            _listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            _listenSocket.Bind(endPoint);
+
+            // 작업개시
+            _listenSocket.Listen(backlog);
+
+            for(int i =0; i<register; i++)
+            {
+                SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+                args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+                RegisterAccept(args);
+            }
+        }
+
+        public void RegisterAccept(SocketAsyncEventArgs args)
+        {
+            // 재등록 오류 방지
+            args.AcceptSocket = null;
+            
+            // 비동기 큐 할당
+            bool pending = _listenSocket.AcceptAsync(args);
+
+            if (pending == false)
+                OnAcceptCompleted(null, args);
+        }
+
+        private void OnAcceptCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            if(args.SocketError == SocketError.Success)
+            {
+                // 세션 생성
+
+                // 연결
+            }
+            else
+                Console.WriteLine(args.SocketError.ToString());
+
+            RegisterAccept(args);
+        }
+    }
+}
