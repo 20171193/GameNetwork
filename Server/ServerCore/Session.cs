79e81219b233bd87622bce4ef258061343a3aa53
심재천 <shimnaldo98@gmail.com>
2025-05-28 18:01:17 +0900

    feat : Session 구현_1  - Send, RegisterSend

--- Server/ServerCore/Session.cs
+++ Server/ServerCore/Session.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices.ObjectiveC;
+using System.Net;
 
 namespace ServerCore
 {
@@ -15,6 +17,128 @@ namespace ServerCore
         private Socket _socket;
         private int _disconnected = 0;
 
+        private RecvBuffer _recvBuffer = new RecvBuffer(65535);
 
+        private object _lock = new object();
+
+        // Send Args
+        private Queue<ArraySegment<byte>> _sendQueue = new Queue<ArraySegment<byte>>();
+        private List<ArraySegment<byte>> _pendingList = new List<ArraySegment<byte>>();
+        private SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs();
+
+        // Receive Args
+        private SocketAsyncEventArgs _recvArgs = new SocketAsyncEventArgs();
+
+        public abstract void OnConnected(EndPoint endPoint);
+        public abstract void OnDisconnected(EndPoint endPoint);
+        public abstract int OnRecv(ArraySegment<byte> buffer);
+        public abstract void OnSend(int numOfBytes);
+
+        private void Clear()
+        {
+            lock(_lock)
+            {
+                _sendQueue.Clear();
+                _pendingList.Clear();
+            }
+        }
+
+        public void Start(Socket socket)
+        {
+            // 초기세팅
+            _socket = socket;
+            _recvArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
+            _sendArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnSendCompleted);
+            // Recevie 등록
+            RegisterRecv();
+        }
+
+        public void Send(List<ArraySegment<byte>> sendBuffList)
+        {
+            // Disconnect 방지
+            if (sendBuffList.Count == 0)
+                return;
+
+            lock(_lock)
+            {
+                foreach (ArraySegment<byte> sendBuff in sendBuffList)
+                    _sendQueue.Enqueue(sendBuff);
+
+                if (_pendingList.Count == 0)
+                    RegisterSend();
+            }
+        }
+        public void Send(ArraySegment<byte> sendBuff)
+        {
+            lock (_lock)
+            {
+                _sendQueue.Enqueue(sendBuff);
+                if (_pendingList.Count == 0)
+                    RegisterSend();
+            }
+        }
+
+        public void Disconnect()
+        {
+            if (Interlocked.Exchange(ref _disconnected, 1) == 1)
+                return;
+
+            OnDisconnected(_socket.RemoteEndPoint);
+            _socket.Shutdown(SocketShutdown.Both);
+            _socket.Close();
+
+            Clear();
+        }
+
+        #region 네트워크 통신
+        private void RegisterSend()
+        {
+            if (_disconnected == 1)
+                return;
+
+            while(_sendQueue.Count > 0)
+            {
+                ArraySegment<byte> buff = _sendQueue.Dequeue();
+
+                _pendingList.Add(buff);
+            }
+            _sendArgs.BufferList = _pendingList;
+
+            try
+            {
+                bool pending = _socket.SendAsync(_sendArgs);
+                if (pending == false)
+                    OnSendCompleted(null, _sendArgs);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"RegisterSend Failed : {ex}");
+            }
+        }
+        private void OnSendCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            lock(_lock)
+            {
+                if(args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
+                {
+                    try
+                    {
+                        _sendArgs.BufferList = null;
+                        _pendingList.Clear();
+
+                        OnSend(_sendArgs.BytesTransferred);
+
+                        if (_sendQueue.Count > 0)
+                            RegisterSend();
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine($"OnSendCompleted Failed : {ex}");
+                    }
+                }
+            }
+        }
+
+        #endregion
     }
 }
