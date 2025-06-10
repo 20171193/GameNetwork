8ad0ae8479dc72d4af302fc36b3f366cc2572742
심재천 <shimnaldo98@gmail.com>
2025-06-10 21:04:09 +0900

    feat : SessionManager 구현 (객체처리 중앙화)

---
+++ Server/DummyClient/Session/ServerSession.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using ServerCore;
+
+namespace DummyClient
+{
+    class ServerSession : PacketSession
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            ;
+        }
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            ;
+        }
+
+        public override void OnRecvPacket(ArraySegment<byte> buffer)
+        {
+            // Todo - PacketManager 연동    
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            ;
+        }
+    }
+}
---
+++ Server/DummyClient/Session/SessionManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DummyClient.Session
+{
+    class SessionManager
+    {
+        static SessionManager _session = new SessionManager();
+        public static SessionManager Instance { get { return _session; } }
+
+        List<ServerSession> _sessions = new List<ServerSession>();
+
+        object _lock = new object();
+
+        public ServerSession Generate()
+        {
+            lock(_lock)
+            {
+                ServerSession session = new ServerSession();
+                _sessions.Add(session);
+                return session;
+            }
+        }
+
+        public void SendForEach()
+        {
+            // Todo - 모든 서버 세션에 패킷 전달
+        }
+    }
+}
