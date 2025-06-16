d5260d58bc2e29584379366c5697fcb48e7c73af
심재천 <shimnaldo98@gmail.com>
2025-06-16 18:22:21 +0900

    feat : Client Session (테스트 이전)

--- Server/Server/Session/ClientSession.cs
+++ Server/Server/Session/ClientSession.cs
@@ -10,18 +10,35 @@ namespace Server
     {
         public int SessionId { get; set; }
         public GameRoom Room { get; set; }
+        public float PosX { get; set; }
+        public float PosY { get; set; }
+        public float PosZ { get; set; }
 
         public override void OnConnected(EndPoint endPoint)
         {
+            Console.WriteLine($"Client OnConnected : {endPoint}");
+
+            Program.Room.Push(() => Program.Room.Enter(this));
         }
 
         public override void OnRecvPacket(ArraySegment<byte> buffer)
         {
-
+            PacketManager.Instance.OnRecvPacket(this, buffer);
         }
 
         public override void OnDisconnected(EndPoint endPoint)
         {
+            SessionManager.Instance.Remove(this);
+
+            if(Room != null)
+            {
+                GameRoom room = Room;
+                room.Push(() => room.Leave(this));
+                // Action 참조 null
+                Room = null;
+            }
+
+            Console.WriteLine($"OnDisconnected : {endPoint}");
         }
 
         public override void OnSend(int numOfBytes)
