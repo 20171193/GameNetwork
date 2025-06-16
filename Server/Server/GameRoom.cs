75943f92cb8f2c27ebf4d755b0e025e27521d104
심재천 <shimnaldo98@gmail.com>
2025-06-16 18:39:54 +0900

    feat : GameRoom (테스트 이전)

--- Server/Server/GameRoom.cs
+++ Server/Server/GameRoom.cs
@@ -37,7 +37,25 @@ namespace Server
             _session.Add(session);
             session.Room = this;
 
-            // 브로드캐스팅
+            // 모든 플레이어 목록 전송
+            S_PlayerList players = new S_PlayerList();
+            foreach(ClientSession s in _session)
+            {
+                players.players.Add(new S_PlayerList.Player()
+                {
+                    isSelf = (s == session),
+                    playerId = s.SessionId,
+                    posX = s.PosX,
+                    posY = s.PosY,
+                    posZ = s.PosZ,
+                });
+            }
+            session.Send(players.Write());
+
+            // 신규 플레이어 입장 브로드캐스팅
+            S_BroadcastEnterGame enter = new S_BroadcastEnterGame();
+            enter.playerId = session.SessionId;
+            enter.posX = 0;
         }
 
         public void Leave(ClientSession session)
@@ -46,6 +64,24 @@ namespace Server
             _session.Remove(session);
 
             // 브로드캐스팅
+            S_BroadcastLeaveGame leave = new S_BroadcastLeaveGame();
+            leave.playerId = session.SessionId;
+            Broadcast(leave.Write());
+        }
+
+        public void Move(ClientSession session, C_Move packet)
+        {
+            // 좌표 변경
+            session.PosX = packet.posX;
+            session.PosY = packet.posY;
+            session.PosZ = packet.posZ;
+
+            // 브로드캐스팅
+            S_BroadcastMove move = new S_BroadcastMove();
+            move.playerId = session.SessionId;
+            move.posX = session.PosX;
+            move.posY = session.PosY;
+            move.posZ = session.PosZ;
         }
     }
 }
